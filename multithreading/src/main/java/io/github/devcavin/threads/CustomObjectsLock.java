23901e3d3dc9fff267e31de61039dfcca95799df
Cavin <cavinlarry001@gmail.com>
2026-05-22 21:41:34 +0300

    update multithreading

--- multithreading/src/main/java/io/github/devcavin/threads/CustomObjectsLock.java
+++ multithreading/src/main/java/io/github/devcavin/threads/CustomObjectsLock.java
@@ -1,42 +1,33 @@
 package io.github.devcavin.threads;
 
-public class ThreadSynchronization {
+public class CustomObjectsLock {
     private static int counter1 = 0;
     private static int counter2 = 0;
 
     public static void main(String[] args) {
-        Thread one = new Thread(() -> {
+        Thread t1 = new Thread(() -> {
             for (int i = 0; i < 10000; i++) {
                 increment1();
             }
         });
 
-        Thread two = new Thread(() -> {
+        Thread t2 = new Thread(() -> {
             for (int i = 0; i < 10000; i++) {
                 increment2();
             }
         });
 
-        one.start();
-        two.start();
+        t1.start();
+        t2.start();
 
         try {
-            one.join();
-            two.join();
+            t1.join();
+            t2.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        System.out.println("Counter2 " + counter1);
-        System.out.println("Counter2 " + counter2);
-    }
-
-
-    private synchronized static void increment1() {
-        counter1++;
-    }
-
-    private synchronized static void increment2() {
-        counter2++;
+        System.out.println("Counter 1: " + counter1);
+        System.out.println("Counter 2: " + counter2);
     }
 }
